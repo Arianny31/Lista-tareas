380cf389abb38bee6c21118ff3a88c4b033942bb
Arianny Rossis <rossisarianny@gmail.com>
2024-07-28 19:58:40 -0400

    Creacion y modificaciones de todo.store,js

---
+++ src/store/todo.store.js
@@ -0,0 +1,30 @@
+import { Todo } from '../todo/models/todo.models';
+
+const Filters ={
+    All:' Todo',
+    Completed: 'Completo',
+    Pending: 'Pendiente'
+}
+
+
+const state = {
+    todos: [
+        new Todo('Piedra del alma'),
+        new Todo('Hola'),
+        new Todo('adios, espero funcione')
+    ],
+    filter: Filters.All
+}
+
+
+const initStore =() =>{
+
+    //console.log(state);
+    console.log ('InitStore');
+
+}
+
+export default{
+    initStore, 
+
+}
