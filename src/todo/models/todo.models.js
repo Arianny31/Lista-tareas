207ecc5dbfb7c34d432afaa9880ccd8ebe30b142
Arianny Rossis <rossisarianny@gmail.com>
2024-07-28 19:06:31 -0400

    Primer models en la carpeta todos

---
+++ src/todo/models/todo.models.js
@@ -0,0 +1,19 @@
+
+
+
+export class todo{
+
+    /**
+     * 
+     * @param {String} descricion 
+     */
+
+
+    constructor( descricion ){
+        this.id =1;
+        this.descricion= descricion;
+        this.done = false;
+        this.createdAt= new Date();
+        
+    }
+}
