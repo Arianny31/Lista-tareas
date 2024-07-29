19de9938cc07409f2988eb715f729a5d5872fff7
Arianny Rossis <rossisarianny@gmail.com>
2024-07-29 17:56:41 -0400

    modificaciones e importaciones

--- src/todo/app.js
+++ src/todo/app.js
@@ -1,4 +1,10 @@
 import html from './app.html?raw';
+import todoStore, {  } from '../store/todo.store';
+import { renderTodos } from './use-case/render-todos';
+
+const ElementId ={
+    TodoList: '.todo-list',
+}
 
 /**
  * 
@@ -8,10 +14,16 @@ import html from './app.html?raw';
 
 export const  App = (elementId) =>{
 
+    const displayTodos= ()=>{
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter());
+        renderTodos( ElementId.TodoList, todos )
+    }
+
     (() =>{
 
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
     })()
 }
